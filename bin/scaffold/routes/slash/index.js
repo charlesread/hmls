69293d05bb4918e8a226249a142149a76b8fae0f
Charles Read <physixrox@gmail.com>
2018-01-20 07:26:23 -0500

    updated scaffold files to be more sane

--- bin/scaffold/routes/slash/index.js
+++ bin/scaffold/routes/slash/index.js
@@ -4,11 +4,16 @@ module.exports = {
   method: 'get',
   path: '/',
   handler: async function (req, h) {
-    const page = require('~/pages/slash/index.marko')
-    return page.stream(
-      {
-        now: new Date()
-      }
-    )
+    try {
+      const page = require('~/pages/slash/index.marko')
+      return page.stream(
+        {
+          now: new Date()
+        }
+      )
+    } catch (err) {
+      console.error(err.message)
+      return err.message
+    }
   }
 }
